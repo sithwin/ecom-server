c19506c8441962da040c1de39ce48cba7a2badd6
Si Thu Win <sthuwin@gmail.com>
2021-10-21 22:19:55 +0800

    created and updated user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,24 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-    data: "hit create or update user",
-  });
+const User = require("../models/user");
+
+exports.createOrUpdateUser = async (req, res) => {
+  const { name, picture, email } = req.user;
+
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name, picture },
+    { new: true }
+  );
+
+  if (user) {
+    console.log("user updated", user);
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      email,
+      name,
+      picture,
+    }).save();
+    console.log("user created", user);
+    res.json(newUser);
+  }
 };
