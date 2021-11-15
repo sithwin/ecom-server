2a0a845355fadef4e6eaa5f70e9e2f0ca0a40d1e
Si Thu Win <sthuwin@gmail.com>
2021-11-15 22:25:08 +0800

    created category route

---
+++ routes/category.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller
+const {
+  create,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/category");
+
+// routes
+router.get("/categories", list);
+router.get("/categories/:slug", authCheck, adminCheck, read);
+router.post("/categories", authCheck, adminCheck, create);
+router.put("/categories/:slug", authCheck, adminCheck, update);
+router.delete("/categories/:slug", authCheck, adminCheck, remove);
+
+module.exports = router;
